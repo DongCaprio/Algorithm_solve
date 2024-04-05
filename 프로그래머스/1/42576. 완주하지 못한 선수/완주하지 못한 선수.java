33ce39912f7916b8407e970e33490a35b933c209
김동진 <dongjin403@naver.com>
2024-04-05 11:42:51 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 66.61 ms, Memory: 95.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        HashMap<String, Integer> map = new HashMap<>();
+        for(String x : participant) {
+        	map.put(x, map.getOrDefault(x, 0)+1);
+        }
+        for(String x : completion) {
+        	map.put(x, map.get(x)-1);
+        }
+        for(String x : map.keySet()) {
+        	if(map.get(x) ==1 ) {
+        		return  x;
+        	}
+        }
+        return answer;
+    }
+}
