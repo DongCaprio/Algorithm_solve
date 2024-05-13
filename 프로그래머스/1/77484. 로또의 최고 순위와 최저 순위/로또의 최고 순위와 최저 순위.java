34e84e6838b1ddc4871ce65247fc324ca46e0ae5
김동진 <dongjin403@naver.com>
2024-05-13 14:12:40 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.09 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -15,46 +15,17 @@ class Solution {
         	if(win_list.contains(x)) min++;
         }
         
-        switch (max) {
-        case 6:
-            answer[0] = 1;
-            break;
-        case 5:
-        	answer[0] = 2;
-            break;
-        case 4:
-        	answer[0] = 3;
-            break;
-        case 3:
-        	answer[0] = 4;
-            break;
-        case 2:
-        	answer[0] = 5;
-            break;
-        default:
-        	answer[0] = 6;
-            break;
-        }
-        switch (min) {
-        case 6:
-        	answer[1] = 1;
-        	break;
-        case 5:
-        	answer[1] = 2;
-        	break;
-        case 4:
-        	answer[1] = 3;
-        	break;
-        case 3:
-        	answer[1] = 4;
-        	break;
-        case 2:
-        	answer[1] = 5;
-        	break;
-        default:
-        	answer[1] = 6;
-        	break;
-        }
+       answer[0] = getGrade(max);
+       answer[1] = getGrade(min);
+        
         return answer;
+    }
+    
+    public int getGrade(int score) {
+    	 if(score >1) {
+         	return 7-score;
+         }else {
+        	 return 6;
+         }
     }
 }
