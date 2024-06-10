4d377ca57d5af2e22d331dbe22736c8fe69176be
김동진 <dongjin403@naver.com>
2024-06-10 13:56:36 +0900

    [level 2] Title: 구명보트, Time: 7.65 ms, Memory: 54.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0, index = 0;
+        Arrays.sort(people);
+        
+        for(int i=people.length-1; i >= index; i--) {
+        	if(people[i] + people[index] <= limit) index++;
+        	answer++;
+        }
+        return answer;
+    }
+}
