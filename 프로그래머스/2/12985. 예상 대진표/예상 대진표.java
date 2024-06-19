7e57f144f76d742449aeb8274fc8888d2f1fcb29
김동진 <dongjin403@naver.com>
2024-06-19 19:20:10 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -5,14 +5,12 @@ class Solution
         int answer = 0;
         while(true){
             answer++;
-            if(a%2==1) a = a+1;
-            if(b%2==1) b = b+1;
+            if(a%2==1) a++;
+            if(b%2==1) b++;
             if(a==b) break;
-            a = a/2;
-            b = b/2;
+            a /= 2;
+            b /= 2;
         }
-
-
         return answer;
     }
 }
