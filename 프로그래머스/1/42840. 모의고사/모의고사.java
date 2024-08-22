df13ecc9d2cd40d0e6e5a3a155b1981c4b1ee0d1
김동진 <dongjin403@naver.com>
2024-08-22 20:22:42 +0900

    [level 1] Title: 모의고사, Time: 5.45 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,47 +1,30 @@
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] answers) {
-        int[] answer = {};
-        
-        int[] answer1 = {1, 2, 3, 4, 5};
-        int[] answer2 = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] answer3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        
-        int count1 = 0;
-        int count2 = 0;
-        int count3 = 0;
-        
-        for(int i=0; i<answers.length; i++) {
-        	int correct = answers[i];
-        	if(correct == answer1[i%answer1.length]) {
-        		count1++;
-        	}
-        	if(correct == answer2[i%answer2.length]) {
-        		count2++;
-        	}
-        	if(correct == answer3[i%answer3.length]) {
-        		count3++;
-        	}
+        int[][] pattern = {
+                {1, 2, 3, 4, 5},
+                {2, 1, 2, 3, 2, 4, 2, 5},
+                {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+        };
+        int[] scores = new int[3];
+
+        for(int i=0; i<answers.length; i++){
+            for(int j=0; j< pattern.length; j++){
+                if(answers[i] == pattern[j][i % pattern[j].length]){
+                    scores[j]++;
+                }
+            }
         }
-        
-        int highestScore = Math.max(Math.max(count1, count2), count3);
-        List<Integer> highestScoringStudents = new ArrayList<>();
-        if (count1 == highestScore) {
-            highestScoringStudents.add(1);
+
+        int max = Arrays.stream(scores).max().getAsInt();
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=0; i<scores.length; i++){
+            if(scores[i] == max) list.add(i+1);
         }
-        if (count2 == highestScore) {
-            highestScoringStudents.add(2);
-        }
-        if (count3 == highestScore) {
-            highestScoringStudents.add(3);
-        }
-        
-        answer = highestScoringStudents.stream()
-                .mapToInt(Integer::intValue)
-                .toArray();
-        
-        return answer;
+
+        return list.stream().sorted().mapToInt(Integer::intValue).toArray();
     }
+    
 }
