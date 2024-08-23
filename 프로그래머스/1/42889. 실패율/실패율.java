0652ab17a436d0c95077083ec83dfbd5873c4e06
김동진 <dongjin403@naver.com>
2024-08-23 22:13:41 +0900

    [level 1] Title: 실패율, Time: 9.50 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,29 +1,24 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 class Solution {
     public int[] solution(int N, int[] stages) {
-        int[] answer = new int[N];
-        int[] sumArr = new int[N+1];
-        int[] countArr = new int[N+1];
-        Map<Integer, Double> map = new HashMap<Integer, Double>();
+        // 각 스테이지에 몇명 있는지 구한다.
+        int[] challenger = new int[N +2];
         for(int i=0; i<stages.length; i++){
-            int val = stages[i];
-            if(val < countArr.length) {
-        		countArr[val] = countArr[val]+1; 
-        	}
-            for(int j=1; j<=val; j++){
-                if(j>N) continue;
-                sumArr[j] = sumArr[j]+1;
+            challenger[stages[i]] ++;
+        }
+        // 스테이지별 실패율을 구한다.
+        HashMap<Integer, Double> map = new HashMap<>();
+        double count = stages.length;
+        for(int i=1; i<=N; i++){
+            if(challenger[i] == 0){
+                map.put(i, (double) 0);
+            }else{
+                map.put(i, challenger[i]/count);
+                count -= challenger[i];
             }
         }
-        for(int i=1; i<N+1; i++) {
-            if(sumArr[i] == 0) map.put(i, 0.0);
-            else map.put(i, (double)countArr[i]/sumArr[i]);                     
-        }
-       
+
         List<Integer> list = new ArrayList<>(map.keySet());
         list.sort((o1, o2) -> Double.compare(map.get(o2), map.get(o1)));
         return list.stream().mapToInt(i -> i).toArray();
