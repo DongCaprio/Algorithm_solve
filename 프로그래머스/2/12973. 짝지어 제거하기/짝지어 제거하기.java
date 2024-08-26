057fd3f41b7252bf1e68d0a2a00bc9e163e6c4bf
김동진 <dongjin403@naver.com>
2024-08-26 23:46:23 +0900

    [level 2] Title: 짝지어 제거하기, Time: 165.35 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -2,15 +2,16 @@ import java.util.Stack;
 
 class Solution {
     public int solution(String s) {
-        int answer = 0;
+        int answer = 1;
         Stack<Character> st = new Stack<>();
-        st.push(s.charAt(0));
-
-        for(int i=1; i<s.length(); i++){
-            if(!st.empty() && st.peek() == s.charAt(i)) st.pop();
-            else st.push(s.charAt(i));
+        for(int i=0; i<s.length(); i++){
+            if(st.isEmpty()) st.push(s.charAt(i));
+            else{
+                if(st.peek() == s.charAt(i)) st.pop();
+                else st.push(s.charAt(i));
+            }
         }
-        if(st.empty()) return 1;
+        if(!st.isEmpty()) answer = 0;
         return answer;
     }
 }
