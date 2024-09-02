acb12bc96a18c0d967286d14d2188f8959ad23a8
김동진 <dongjin403@naver.com>
2024-09-02 20:48:57 +0900

    [level 1] Title: 카드 뭉치, Time: 0.10 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -1,18 +1,25 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
         String answer = "Yes";
-        int idx1=0, idx2=0;
+        Deque<String> d1 = new ArrayDeque<>();
+        Deque<String> d2 = new ArrayDeque<>();
+        for(String x : cards1) d1.add(x);
+        for(String x : cards2) d2.add(x);
+
         for(String x : goal){
-            if(idx1< cards1.length && x.equals(cards1[idx1])){
-                idx1++;
-            }
-            else if(idx2< cards2.length && x.equals(cards2[idx2])){
-                idx2++;
-            }
-            else{
+            if(!d1.isEmpty() && d1.peek().equals(x)){
+                d1.remove();
+            }else if(!d2.isEmpty() &&d2.peek().equals(x)){
+                d2.remove();
+
+            }else{
                 return "No";
             }
         }
+
         return answer;
     }
 }
