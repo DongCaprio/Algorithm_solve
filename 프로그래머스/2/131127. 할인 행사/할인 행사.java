d0491f2d186b04035ebee2d18b52fdfebe1d726a
김동진 <dongjin403@naver.com>
2024-09-03 23:43:04 +0900

    [level 2] Title: 할인 행사, Time: 49.70 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,26 +1,32 @@
 import java.util.HashMap;
-import java.util.Map;
 
 class Solution {
-    public int solution(String[] want, int[] number, String[] discount) {
+    public static int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
-        Map<String, Integer> map = new HashMap<>();
-        for(int i=0; i<discount.length; i++){
-            if(i<9){
-                map.put(discount[i], map.getOrDefault(discount[i], 0)+1);
-            }else{
-                map.put(discount[i], map.getOrDefault(discount[i], 0)+1);
-                boolean flag = true;
-                for(int j=0; j<want.length; j++){
-                    if(number[j] != map.getOrDefault(want[j],0)){
-                        flag = false;
-                        break;
-                    }
-                }
-                if(flag) answer++;
-                map.put(discount[i-9], map.getOrDefault(discount[i-9], 0)-1);
-            }
+        HashMap<String, Integer> map = new HashMap<>();
+        HashMap<String, Integer> map2 = new HashMap<>();
+        for(int i=0; i<want.length; i++){
+            map.put(want[i], number[i]);
         }
+
+        for(int i=0; i<10; i++){
+            map2.put(discount[i], map2.getOrDefault(discount[i], 0) + 1);
+        }
+        if(map.equals(map2)) answer++;
+        for(int i=10; i<discount.length; i++){
+            map2.put(discount[i], map2.getOrDefault(discount[i], 0) + 1);
+            map2.put(discount[i-10], map2.get(discount[i-10])-1);
+            if(map2.get(discount[i-10]) == 0) map2.remove(discount[i - 10]);
+            if(map.equals(map2)) answer++;
+        }
+
         return answer;
+    }
+
+    public static void main(String[] args) {
+        String[] want = {"banana", "apple", "rice", "pork", "pot"};
+        int[] number = {3, 2, 2, 2, 1};
+        String[] discount = {"chicken", "apple", "apple", "banana", "rice", "apple", "pork", "banana", "pork", "rice", "pot", "banana", "apple", "banana"};
+        solution(want, number, discount);
     }
 }
