2a012411d04be1b1e9751820f48074f3a184947d
김동진 <dongjin403@naver.com>
2025-02-05 11:48:08 +0900

    [level 2] Title: 전화번호 목록, Time: 310.15 ms, Memory: 99.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        
+        Arrays.sort(phone_book);
+        
+        for(int i=0; i<phone_book.length-1; i++) {
+        	String str1 = phone_book[i];
+        	String str2 = phone_book[i+1];
+        	if(str2.startsWith(str1)) {
+    			return false;
+    		}
+        }
+        return answer;
+    }
+}
