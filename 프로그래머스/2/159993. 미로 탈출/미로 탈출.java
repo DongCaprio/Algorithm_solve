55fd788296c46d93896195956ea5805b1996a1f0
김동진 <dongjin403@naver.com>
2025-03-24 21:43:52 +0900

    [level 2] Title: 미로 탈출, Time: 8.50 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
@@ -3,81 +3,80 @@ import java.util.Queue;
 
 class Solution {
 
-    int[][] map;
-    int[][] moveArray = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-
     class Node {
         int x;
         int y;
-        int dist;
 
-        public Node(int x, int y, int dist) {
+        public Node(int x, int y) {
             this.x = x;
             this.y = y;
-            this.dist = dist;
         }
     }
 
-    public int bfs(int startX, int startY, char target) {
-        boolean[][] visited = new boolean[map.length][map[0].length];
+    Node lever, start, end;
+    String[] maps;
+    int[][] move = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    int xLength, yLength;
+
+    public int solution(String[] maps) {
+        this.maps = maps;
+        xLength = maps[0].length();
+        yLength = maps.length;
+
+        for (int i = 0; i < yLength; i++) {
+            for (int j = 0; j < xLength; j++) {
+                if (maps[i].charAt(j) == 'L') {
+                    lever = new Node(j, i);
+                }
+                if (maps[i].charAt(j) == 'S') {
+                    start = new Node(j, i);
+                }
+                if (maps[i].charAt(j) == 'E') {
+                    end = new Node(j, i);
+                }
+            }
+        }
+
+        int toLever = bfs(start, lever);
+        if (toLever == -1) {
+            return -1;
+        }
+        int toEnd = bfs(lever, end);
+        if (toEnd == -1) {
+            return -1;
+        }
+
+        return toLever + toEnd;
+    }
+
+
+    public int bfs(Node start, Node target) {
+        int[][] dist = new int[yLength][xLength];
         Queue<Node> Q = new LinkedList<>();
-        Q.offer(new Node(startX, startY, 0));
-        visited[startY][startX] = true;
+        dist[start.y][start.x] = 1;
+        Q.offer(start);
 
         while (!Q.isEmpty()) {
-            Node current = Q.poll();
+            Node now = Q.poll();
+            for (int[] movee : move) {
+                int nx = movee[0] + now.x;
+                int ny = movee[1] + now.y;
 
-            if (map[current.y][current.x] == target) {
-                return current.dist;
-            }
-
-            for (int[] move : moveArray) {
-                int nx = current.x + move[0];
-                int ny = current.y + move[1];
-
-                if (nx >= map[0].length || ny >= map.length ||
-                        nx < 0 || ny < 0 ||
-                        map[ny][nx] == 'X' || visited[ny][nx]) {
+                if (nx < 0 || ny < 0 || nx >= xLength || ny >= yLength ||
+                        maps[ny].charAt(nx) == 'X' ||
+                        dist[ny][nx] > 0) {
                     continue;
                 }
 
-                visited[ny][nx] = true;
-                Q.offer(new Node(nx, ny, current.dist + 1));
+                dist[ny][nx] = dist[now.y][now.x] + 1;
+                Q.offer(new Node(nx, ny));
+
+                if (nx == target.x && ny == target.y) {
+                    return dist[ny][nx] - 1;
+                }
             }
         }
+
         return -1;
-    }
-
-
-    public int solution(String[] maps) {
-        int startX = 0;
-        int startY = 0;
-        int leverX = 0;
-        int leverY = 0;
-
-        map = new int[maps.length][maps[0].length()];
-
-        for (int i = 0; i < maps.length; i++) {
-            char[] charArray = maps[i].toCharArray();
-            for (int j = 0; j < charArray.length; j++) {
-                map[i][j] = charArray[j];
-                if (charArray[j] == 'S') {
-                    startY = i;
-                    startX = j;
-                }
-                if (charArray[j] == 'L') {
-                    leverY = i;
-                    leverX = j;
-                }
-
-            }
-        }
-
-        int leverCount = bfs(startX, startY, 'L');
-        if (leverCount == -1) return -1;
-        int exitCount = bfs(leverX, leverY, 'E');
-        if (exitCount == -1) return -1;
-
-        return leverCount + exitCount;
     }
 }
