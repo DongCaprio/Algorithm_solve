366dfc9d9fa8d215660674e81a696992773f4dee
김동진 <dongjin403@naver.com>
2025-03-27 15:58:48 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 4.64 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
@@ -2,29 +2,31 @@ import java.util.PriorityQueue;
 
 class Solution {
 
-    int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    int[][][] cost;
+    int[][] direction = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
     class Node {
         int x;
         int y;
-        int direction;
+        int dir;
         int cost;
 
-        public Node(int x, int y, int direction, int cost) {
+        public Node(int x, int y, int dir, int cost) {
             this.x = x;
             this.y = y;
-            this.direction = direction;
+            this.dir = dir;
             this.cost = cost;
         }
     }
 
+
     public int solution(int[][] board) {
         int n = board.length;
-        int[][][] goInfo = new int[n][n][4];
+        cost = new int[n][n][direction.length];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                for (int k = 0; k < 4; k++) {
-                    goInfo[i][j][k] = Integer.MAX_VALUE;
+                for (int k = 0; k < direction.length; k++) {
+                    cost[i][j][k] = Integer.MAX_VALUE;
                 }
             }
         }
@@ -32,36 +34,37 @@ class Solution {
         PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> o1.cost - o2.cost);
         pq.offer(new Node(0, 0, -1, 0));
 
-        int minValue = Integer.MAX_VALUE;
+        int minCost = Integer.MAX_VALUE;
 
         while (!pq.isEmpty()) {
-            Node now = pq.poll();
-            int x = now.x, y = now.y, direction = now.direction, cost = now.cost;
 
-            if (y == n - 1 && x == n - 1) {
-                minValue = Math.min(minValue, now.cost);
+            Node now = pq.poll();
+
+            if (now.y == n - 1 && now.x == n - 1) {
+                minCost = Math.min(minCost, now.cost);
             }
 
-            for (int i = 0; i < 4; i++) {
-                int nx = x + directions[i][0];
-                int ny = y + directions[i][1];
+            for (int newDir = 0; newDir < direction.length; newDir++) {
+                int nx = direction[newDir][0] + now.x;
+                int ny = direction[newDir][1] + now.y;
 
-                if (nx < 0 || ny < 0 || nx >= n || ny >= n ||
-                        board[ny][nx] == 1) {
+                if (nx < 0 || ny < 0 || nx >= n || ny >= n || board[ny][nx] == 1) {
                     continue;
                 }
 
-                int newCost = cost + 100;
-                if (direction != -1 && direction != i) {
+                int newCost = now.cost + 100;
+                if (now.dir != -1 && now.dir != newDir) {
                     newCost += 500;
                 }
-                if (newCost < goInfo[ny][nx][i]) {
-                    goInfo[ny][nx][i] = newCost;
-                    pq.offer(new Node(nx, ny, i, newCost));
+
+                if (newCost < cost[ny][nx][newDir]) {
+                    cost[ny][nx][newDir] = newCost;
+                    pq.offer(new Node(nx, ny, newDir, newCost));
                 }
             }
+
         }
 
-        return minValue;
+        return minCost;
     }
 }
