a330485e821bc7e12d8386bccc5e757121378df0
김동진 <dongjin403@naver.com>
2025-04-02 19:32:54 +0900

    [level 1] Title: K번째수, Time: 1.37 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -3,19 +3,14 @@ import java.util.Arrays;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-        for(int i=0; i<commands.length; i++){
-            int lt=0, rt=0;
-            for(int j=0; j<commands[i].length; j++){
-                if(j==0) lt = commands[i][j];
-                else if(j==1) rt = commands[i][j];
-                else{
-                    int[] newArr = Arrays.copyOfRange(array, lt-1, rt);
-                    Arrays.sort(newArr);
-                    answer[i] = newArr[commands[i][j]-1];
-                }
-            }
+
+        for (int i = 0; i < commands.length; i++) {
+            int[] cutArray = Arrays.copyOfRange(array,
+                    commands[i][0] - 1, commands[i][1]);
+            Arrays.sort(cutArray);
+            answer[i] = cutArray[commands[i][2] - 1];
         }
-        
+
         return answer;
     }
 }
