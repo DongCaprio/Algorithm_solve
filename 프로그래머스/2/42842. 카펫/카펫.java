a57c8808c6c174822b8a33fb98be67b63e695577
김동진 <dongjin403@naver.com>
2025-04-14 14:54:19 +0900

    [level 2] Title: 카펫, Time: 0.08 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,19 +1,17 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int[] answer = {};
         int total = brown + yellow;
-        
-        for(int width=3; width<= total; width++) {
-        	if(total % width != 0) continue;
-        	int height = total  / width ;
-        	if(height > width) continue;
-        	
-        	int inside_area = (width-2)*(height-2);
-        	if(inside_area == yellow) {
-        		answer = new int[]{width, height};
-        		break;
-        	}
+
+        for (int h = 3; h <= total / 3; h++) {
+            int width = total / h;
+            if (width < h) {
+                continue;
+            }
+            if ((width - 2) * (h - 2) == yellow) {
+                return new int[]{width, h};
+            }
         }
+        int[] answer = {};
         return answer;
     }
 }
