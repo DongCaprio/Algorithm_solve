a660af2e7118d16d7df6ad156e5e6924600f0ec8
김동진 <dongjin403@naver.com>
2025-04-15 19:37:48 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.06 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -1,18 +1,5 @@
-import java.util.*;
-
 public class Solution {
     public int solution(int n) {
-        int ans = 0;
-
-        while(n != 0){
-            if(n % 2 == 0){
-                n /= 2;
-            } else {
-                n--;
-                ans++;
-            }
-        }
-
-        return ans;
+        return Integer.toBinaryString(n).replace("0", "").length();
     }
 }
