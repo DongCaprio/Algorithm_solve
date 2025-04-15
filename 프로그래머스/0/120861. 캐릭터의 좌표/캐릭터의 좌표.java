b64c900788e1725120d4070ca799a94d2df655ab
김동진 <dongjin403@naver.com>
2025-04-15 20:10:31 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.06 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -1,35 +1,32 @@
+import java.util.Map;
+
 class Solution {
     public int[] solution(String[] keyinput, int[] board) {
-        int[] answer = {0, 0};
         int maxX = (board[0] - 1) / 2;
         int maxY = (board[1] - 1) / 2;
 
-        int[][] go = {{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
+        int x = 0, y = 0;
 
-        for (String dir : keyinput) {
-            if (dir.equals("right")) {
-                if (answer[0] + go[0][0] > maxX) {
-                    continue;
-                }
-                answer[0] += go[0][0];
-            } else if (dir.equals("left")) {
-                if (answer[0] + go[1][0] < -(maxX)) {
-                    continue;
-                }
-                answer[0] += go[1][0];
-            } else if (dir.equals("down")) {
-                if (answer[1] + go[2][1] < -(maxY)) {
-                    continue;
-                }
-                answer[1] += go[2][1];
-            } else if (dir.equals("up")) {
-                if (answer[1] + go[3][1] > maxY) {
-                    continue;
-                }
-                answer[1] += go[3][1];
+        // 방향 매핑: key → [dx, dy]
+        Map<String, int[]> moveMap = Map.of(
+                "left", new int[]{-1, 0},
+                "right", new int[]{1, 0},
+                "up", new int[]{0, 1},
+                "down", new int[]{0, -1}
+        );
+
+        for (String key : keyinput) {
+            int[] move = moveMap.get(key);
+            int nx = x + move[0];
+            int ny = y + move[1];
+
+            // 경계 체크
+            if (Math.abs(nx) <= maxX && Math.abs(ny) <= maxY) {
+                x = nx;
+                y = ny;
             }
         }
 
-        return answer;
+        return new int[]{x, y};
     }
 }
