7de2c52ee40b98bf81e59a6dba726ad431c439d5
김동진 <dongjin403@naver.com>
2025-05-05 14:46:00 +0900

    [level 1] Title: 예산, Time: 0.53 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -2,13 +2,18 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int[] d, int budget) {
-    	int answer = 0;
+        int answer = 0;
+        int sum = 0;
+
         Arrays.sort(d);
-        for (int i = 0; i < d.length; i++) {
-            budget -= d[i];
-            if (budget < 0) break;
+        for (int money : d) {
+            sum += money;
+            if (sum > budget) {
+                break;
+            }
             answer++;
         }
+
         return answer;
     }
 }
